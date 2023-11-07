aa2b4e507916d5dc8af39e5089ffff23b4411a5a
masa-laboratory <masa-lab@outlook.com>
2023-11-07 09:59:17 +0800

    Update common_def.h

--- common/common_def.h
+++ common/common_def.h
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <fstream>
+
 extern int kernel_info_m_next_uid;
+
+#define OPEN_MEMORY_TRACE_FILE() \
+  std::ofstream outfile; \
+  outfile.open("memor_trace.txt");
+
+#define CLOSE_MEMORY_TRACE_FILE() \
+  outfile.close();
+
+#define PRINT_2_MEMORY_TRACE_FILE(s) \
+  outfile << std::hex << s << std::endl;
+
+#define PRINT_2_MEMORY_TRACE_FILE_0x() \
+  outfile << "0x";
