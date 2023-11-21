c305490ddfff07603f4e5e721cfb040233704c5d
masa-laboratory <masa-lab@outlook.com>
2023-11-22 00:53:16 +0800

    Update common_def.h

--- common/common_def.h
+++ common/common_def.h
@@ -1,6 +1,21 @@
 #include <iostream>
 #include <fstream>
 
+
+/* User define Start. */
+#define USE_BOOST
+#define gpgpu_concurrent_kernel_sm true
+/* User define End. */
+
+
+#ifdef USE_BOOST
+  /* See https://www.boost.org/doc/libs/1_68_0/doc/html/mpi/tutorial.html for tutorial of boost::mpi,
+   * to use the boost::mpi to implement the multi-process running. */
+  #include <boost/mpi.hpp>
+  #include <boost/serialization/vector.hpp>
+  #include <boost/serialization/map.hpp>
+#endif
+
 extern int kernel_info_m_next_uid;
 
 #define OPEN_MEMORY_TRACE_FILE() \
