550dd01d659326647a73e6e4cc0cdc8cee1f779d
masa-laboratory <masa-lab@outlook.com>
2023-11-29 19:02:31 +0800

    Update common_def.h

--- common/common_def.h
+++ common/common_def.h
@@ -16,6 +16,11 @@
   #include <boost/serialization/map.hpp>
 #endif
 
+
+#ifdef USE_BOOST
+void simple_mpi_test(int argc, char **argv);
+#endif
+
 extern int kernel_info_m_next_uid;
 
 #define OPEN_MEMORY_TRACE_FILE() \
@@ -30,3 +35,20 @@ extern int kernel_info_m_next_uid;
 
 #define PRINT_2_MEMORY_TRACE_FILE_0x() \
   outfile << "0x";
+
+#define START_TIMER(no) auto start##no = std::chrono::system_clock::now();
+
+#define STOP_AND_REPORT_TIMER_pass(pass, no) \
+    if (pass != -1) std::cout << "    pass-" << pass << " "; \
+    auto end##no = std::chrono::system_clock::now(); \
+    auto duration##no = std::chrono::duration_cast<std::chrono::microseconds>(end##no - start##no); \
+    auto cost##no = double(duration##no.count()) * std::chrono::microseconds::period::num / std::chrono::microseconds::period::den; \
+    std::cout << "Cost " << no << "-" << cost##no << " ms." << std::endl;
+
+
+#define STOP_AND_REPORT_TIMER_rank(rank, no) \
+    std::cout << "    L1 rank-" << rank << " "; \
+    auto end##no = std::chrono::system_clock::now(); \
+    auto duration##no = std::chrono::duration_cast<std::chrono::microseconds>(end##no - start##no); \
+    auto cost##no = double(duration##no.count()) * std::chrono::microseconds::period::num / std::chrono::microseconds::period::den; \
+    std::cout << "Cost " << no << "-" << cost##no << " seconds." << std::endl;
