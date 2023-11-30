054c824b5a25466590e5c95fbcb39f6c6f27d7bd
masa-laboratory <masa-lab@outlook.com>
2023-11-30 09:52:28 +0800

    Update parda.h

--- parda/parda.h
+++ parda/parda.h
@@ -62,21 +62,21 @@ extern FILE* pid_fp;
 typedef char HKEY[SLEN];
 
 typedef struct end_keytime_s {
-		narray_t* gkeys;
-		narray_t* gtimes;
+    narray_t* gkeys;
+    narray_t* gtimes;
 }end_keytime_t;
 
 typedef struct processor_info_s {
-	int pid,psize;
-	long tstart,tlen,tend,sum;
+    int pid,psize;
+    long tstart,tlen,tend,sum;
 }processor_info_t;
 
 typedef struct program_data_s {
-  GHashTable* gh;
-	narray_t* ga;
-	end_keytime_t ekt;
-  Tree* root;
-  unsigned int *histogram;
+    GHashTable* gh;
+    narray_t* ga;
+    end_keytime_t ekt;
+    Tree* root;
+    unsigned int *histogram;
 }program_data_t;
 
 void classical_tree_based_stackdist(char* inputFileName, long lines);
@@ -95,7 +95,7 @@ processor_info_t parda_get_processor_info(int pid, int psize, long sum);
 void parda_get_abfront(program_data_t* pdt_a, const narray_t* gb, const processor_info_t* pit_a);
 int parda_get_abend(program_data_t* pdt_b, const end_keytime_t* ekt_a);
 program_data_t parda_merge(program_data_t* pdt_a, program_data_t* pdt_b,
-				const processor_info_t* pit_b);
+                const processor_info_t* pit_b);
 /*functions for parda print*/
 void parda_print_front(const program_data_t* pdt);
 void parda_print_end(const end_keytime_t* ekt);
@@ -116,64 +116,112 @@ double rtclock(void);
 
 /*parda inline functions*/
 static inline T parda_low(int pid, int psize, T sum) {
-	return (((long long)(pid))*(sum)/(psize));
+    return (((long long)(pid))*(sum)/(psize));
 }
 
 static inline T parda_high(int pid, int psize, T sum) {
-		return parda_low(pid + 1, psize, sum)-1;
+    return parda_low(pid + 1, psize, sum)-1;
 }
 
 static inline T parda_size(int pid, int psize, T sum) {
-		return (parda_low(pid + 1, psize, sum)) - (parda_low(pid, psize, sum));
+    return (parda_low(pid + 1, psize, sum)) - (parda_low(pid, psize, sum));
 }
 
 static inline T parda_owner(T index, int psize, T sum) {
-		return (((long long)psize)*(index+1)-1)/sum;
+    return (((long long)psize)*(index+1)-1)/sum;
 }
 
 static inline char* parda_generate_pfilename(char filename[], int pid, int psize) {
-	char pfilename[30];
-	sprintf(pfilename, "%d_%s_p%d.txt", psize, filename, pid);
-	return strdup(pfilename);
+    char pfilename[30];
+    sprintf(pfilename, "%d_%s_p%d.txt", psize, filename, pid);
+    return strdup(pfilename);
 }
 
 static inline void process_one_access(char* input, program_data_t* pdt, const long tim) {
-		int distance;
-		int *lookup;
-		lookup = (T*)g_hash_table_lookup(pdt->gh, input);
+    int distance;
+    int *lookup;
+    lookup = (T*)g_hash_table_lookup(pdt->gh, input);
     //printf("gh=%p process_one\n",pdt->gh);
-		// Cold start: Not in the list yet
-		if (lookup == NULL) {
-				char *data = strdup(input);
-				pdt->root=insert(tim,pdt->root);
-				long *p_data;
+    // Cold start: Not in the list yet
+    if (lookup == NULL) {
+        char *data = strdup(input);
+        pdt->root=insert(tim,pdt->root);
+        long *p_data;
         narray_append_val(pdt->ga,input);
-				if ( !(p_data = (long*)malloc(sizeof(long))) )
-				{
-						printf("no memory for p_data\n");assert(0);exit(-1);
-				}
-				*p_data = tim;
-				g_hash_table_insert(pdt->gh, data, p_data);  // Store pointer to list element
-		}
-		// Hit: We've seen this data before
-		else {
-				char *data = strdup(input);
-				pdt->root = insert((*lookup), pdt->root);
-				distance = node_size(pdt->root->right);
-				pdt->root = delete_(*lookup, pdt->root);
-				pdt->root = insert(tim, pdt->root);
-				int *p_data;
-				if ( !(p_data = (int*)malloc(sizeof(int)))) {
-						printf("no memory for p_data\n");
+        if ( !(p_data = (long*)malloc(sizeof(long))) )
+        {
+            printf("no memory for p_data\n");assert(0);exit(-1);
+        }
+        *p_data = tim;
+        g_hash_table_insert(pdt->gh, data, p_data);  // Store pointer to list element
+    }
+    // Hit: We've seen this data before
+    else {
+        char *data = strdup(input);
+        pdt->root = insert((*lookup), pdt->root);
+        distance = node_size(pdt->root->right);
+        pdt->root = delete_(*lookup, pdt->root);
+        pdt->root = insert(tim, pdt->root);
+        int *p_data;
+        if (!(p_data = (int*)malloc(sizeof(int)))) {
+            printf("no memory for p_data\n");
             assert(0); exit(-1);
-				}
-				*p_data = tim;
-				g_hash_table_replace(pdt->gh, data, p_data);
-				// Is distance greater than the largest bucket
-				if (distance > nbuckets)
-						pdt->histogram[B_OVFL] += 1;
-				else
-						pdt->histogram[distance] += 1;
-		}
+        }
+        *p_data = tim;
+        g_hash_table_replace(pdt->gh, data, p_data);
+        // Is distance greater than the largest bucket
+        if (distance > nbuckets)
+            pdt->histogram[B_OVFL] += 1;
+        else
+            pdt->histogram[distance] += 1;
+    }
+}
+
+
+static inline int process_one_access_and_get_distance(char* input, program_data_t* pdt, const long tim) {
+    int distance;
+    int *lookup;
+    lookup = (T*)g_hash_table_lookup(pdt->gh, input);
+    //printf("gh=%p process_one\n",pdt->gh);
+    // Cold start: Not in the list yet
+    if (lookup == NULL) {
+        char *data = strdup(input);
+        pdt->root=insert(tim,pdt->root);
+        long *p_data;
+        narray_append_val(pdt->ga,input);
+        if ( !(p_data = (long*)malloc(sizeof(long))) )
+        {
+            printf("no memory for p_data\n");assert(0);exit(-1);
+        }
+        *p_data = tim;
+        g_hash_table_insert(pdt->gh, data, p_data);  // Store pointer to list element
+
+        return B_INF; // return distance = B_INF
+    }
+    // Hit: We've seen this data before
+    else {
+        char *data = strdup(input);
+        pdt->root = insert((*lookup), pdt->root);
+        distance = node_size(pdt->root->right);
+        pdt->root = delete_(*lookup, pdt->root);
+        pdt->root = insert(tim, pdt->root);
+        int *p_data;
+        if (!(p_data = (int*)malloc(sizeof(int)))) {
+            printf("no memory for p_data\n");
+            assert(0); exit(-1);
+        }
+        *p_data = tim;
+        g_hash_table_replace(pdt->gh, data, p_data);
+        // Is distance greater than the largest bucket
+        if (distance > nbuckets)
+            pdt->histogram[B_OVFL] += 1;
+        else
+            pdt->histogram[distance] += 1;
+        
+        if (distance > nbuckets)
+            return B_OVFL; // return distance = B_OVFL
+        else
+            return distance; // return distance = diatnce
+    }
 }
 #endif
