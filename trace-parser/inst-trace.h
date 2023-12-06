762d73e8e1e2a5457ef8131ba20b5f674f5f94b5
masa-laboratory <masa-lab@outlook.com>
2023-12-06 14:43:50 +0800

    Update inst-trace.h

--- trace-parser/inst-trace.h
+++ trace-parser/inst-trace.h
@@ -44,7 +44,6 @@ struct inst_trace_t {
 
 
 struct _inst_trace_t {
- public:
   _inst_trace_t();
   _inst_trace_t(const _inst_trace_t &b);
   _inst_trace_t(unsigned _kernel_id, unsigned _pc, std::string _instn_str) {
@@ -98,66 +97,9 @@ struct _inst_trace_t {
 
   std::vector<std::string> get_opcode_tokens() const;
 
-  ~_inst_trace_t();
-  /*********************************************************************/ // TODO
- private:
-  unsigned m_opcode;
-  unsigned m_uid;
-  bool m_empty;
-  bool m_isatomic;
+  std::vector<std::string> get_opcode_tokens_directly() const;
 
-  bool m_decoded = false;
-  address_type pc = (address_type)-1;
-  unsigned isize;   // size of instruction in bytes
-
-  //记录了当前指令的所有目的操作数寄存器ID。
-  unsigned out[8];
-  //记录了当前指令的所有目的操作数寄存器总数。
-  unsigned outcount;
-  //记录了当前指令的所有源操作数寄存器ID。
-  unsigned in[24];
-  //记录了当前指令的所有源操作数寄存器总数。
-  unsigned incount;
-
-  unsigned char is_vectorin;
-  unsigned char is_vectorout;
-
-  int pred;  // predicate register number
-  int ar1, ar2;
-  // register number for bank conflict evaluation
-  struct {
-    int dst[MAX_REG_OPERANDS];
-    int src[MAX_REG_OPERANDS];
-  } arch_reg;
-
-  _memory_op_t memory_op;      // memory_op used by ptxplus
-
-  unsigned num_operands;
-  unsigned num_regs;  // count vector operand as one register operand
-
-  unsigned data_size;  // what is the size of the word being operated on?
-
-  op_type op;       // opcode (uarch visible)
-  special_ops
-      sp_op;  // code (uarch visible) identify if int_alu, fp_alu, int_mul ....
-  mem_operation mem_op;        // code (uarch visible) identify memory type
-
-  bool const_cache_operand;   // has a load from constant memory as an operand
-
-  types_of_operands oprnd_type;  // code (uarch visible) identify if the
-                                 // operation is an interger or a floating point
-
-  bool should_do_atomic;
-  bool m_is_printf;
-  unsigned m_warp_id;
-  unsigned m_dynamic_warp_id;
-  // const core_config *m_config;
-  active_mask_t m_warp_active_mask;  // dynamic active mask for timing model
-                                     // (after predication)
-  memory_space_t space;
-  cache_operator_type cache_op;
-  /*********************************************************************/
-  
+  ~_inst_trace_t();  
 };
 
 #endif
