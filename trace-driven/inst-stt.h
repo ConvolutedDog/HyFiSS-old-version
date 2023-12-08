fbbc08886738a01dc477493080703d13a4209ac9
masa-laboratory <masa-lab@outlook.com>
2023-12-08 21:51:11 +0800

    Add inst-stt.h

---
+++ trace-driven/inst-stt.h
@@ -0,0 +1,57 @@
+
+// Instruction State Transition Table
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <bitset>
+#include <list>
+#include <string.h>
+#include <map>
+#include <string>
+
+#ifndef INST_STT_H
+#define INST_STT_H
+
+typedef bool inst_stage_t;
+
+class inst_stt {
+ public:
+  
+  inst_stt();
+
+
+
+
+
+
+
+
+
+
+ private:
+
+  inst_stage_t fetch_stage;
+
+
+  inst_stage_t wr_bk_stage;
+  inst_stage_t warp_exit_stage;
+
+
+
+
+};
+
+
+
+
+class mem_stat_t {
+ public:
+  mem_stat_t();
+};
+
+
+struct SM_computation_instance {
+};
+
+#endif
