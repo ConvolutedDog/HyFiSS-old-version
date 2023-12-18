461044791c1303a365834857feb2500ef3cf1e66
masa-laboratory <masa-lab@outlook.com>
2023-12-18 16:49:56 +0800

    Update Scoreboard.h

--- hw-component/Scoreboard.h
+++ hw-component/Scoreboard.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <set>
 #include <vector>
+#include <iostream>
 
 #ifndef SCOREBOARD_H
 #define SCOREBOARD_H
@@ -13,25 +14,25 @@ class Scoreboard {
   //构造函数。
   Scoreboard(const unsigned smid, const unsigned n_warps);
   //发射指令时，其目标寄存器将保留在相应硬件warp的记分牌中。
-  void reserveRegisters(const unsigned wid, std::vector<unsigned> regnums, bool is_load);
+  void reserveRegisters(const unsigned wid, std::vector<int> regnums, bool is_load);
   //当指令完成写回时，其目标寄存器将被释放。
-  void releaseRegisters(const unsigned wid, std::vector<unsigned> regnums);
+  void releaseRegisters(const unsigned wid, std::vector<int> regnums);
   //将单个目标寄存器释放。
-  void releaseRegister(const unsigned wid, const unsigned regnum);
+  void releaseRegister(const unsigned wid, const int regnum);
   //检测冒险，检测某个指令使用的寄存器是否被保留在记分板中，如果有的话就是发生了 WAW 或 RAW 冒险。
-  bool checkCollision(const unsigned wid, std::vector<unsigned> regnums, 
-                      unsigned pred, unsigned ar1, unsigned ar2) const;
+  bool checkCollision(const unsigned wid, std::vector<int> regnums, 
+                      int pred, int ar1, int ar2) const;
   //返回记分牌的reg_table中是否有挂起的写入。warp id指向的reg_table为空的话，代表没有挂起的写入，返
   //回false。[挂起的写入]是指wid是否有已发射但尚未完成的指令，将目标寄存器保留在记分牌。
   bool pendingWrites(const unsigned wid) const;
   //打印记分牌的内容。
   void printContents() const;
   
-  const bool islongop(const unsigned wid, const unsigned regnum);
+  const bool islongop(const unsigned wid, const int regnum);
 
  private:
   //将单个目标寄存器保留在相应硬件warp的记分牌中。
-  void reserveRegister(const unsigned wid, const unsigned regnum);
+  void reserveRegister(const unsigned wid, const int regnum);
   //返回SM的ID。
   int get_sid() const { return m_smid; }
   //SM的ID。
@@ -47,14 +48,14 @@ class Scoreboard {
   //指令发射前，就需要将目标寄存器 r0 加入到 reg_table 中。
   //索引: warp id=>reg_id=>挂起的写入计数。这里，每个warp有自己的一个 std::vector reg_table。换句
   //话说，每个warp有一个记分牌。
-  std::vector<std::set<unsigned> > reg_table;
+  std::vector<std::set<int> > reg_table;
 
   // Register that depend on a long operation (global, local or tex memory)
   //跟踪存储器访问的目的寄存器。挂起对寄存器的写入，即如果某条访存指令要写入寄存器 r1，在该条指令发射
   //前，就需要将目标寄存器 r1 加入到 longopregs 中。
   //索引: warp id=>reg_id=>挂起的写入计数。这里，每个warp有自己的一个 std::vector longopregs。换
   //句话说，每个warp有一个记分牌。
-  std::vector<std::set<unsigned> > longopregs;
+  std::vector<std::set<int> > longopregs;
 };
 
 
