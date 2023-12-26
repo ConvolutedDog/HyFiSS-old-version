f6568fe86debe9c9df98c10238b672cb97ce6a90
masa-laboratory <masa-lab@outlook.com>
2023-12-26 20:12:16 +0800

    Update PipelineUnit.cc

---
+++ hw-component/PipelineUnit.cc
@@ -0,0 +1,217 @@
+
+#include "PipelineUnit.h"
+
+pipelined_simd_unit::pipelined_simd_unit(register_set *result_port,
+                                         unsigned max_latency,
+                                         unsigned issue_reg_id, 
+                                         hw_config* hw_cfg, trace_parser* tracer) {
+  m_pipeline_depth = max_latency;
+  m_pipeline_reg.reserve(m_pipeline_depth);
+  for (unsigned i = 0; i < m_pipeline_depth; i++) {
+    m_pipeline_reg.push_back(new inst_fetch_buffer_entry());
+  }
+  m_result_port = result_port;
+  m_issue_reg_id = issue_reg_id;
+  m_dispatch_reg = new inst_fetch_buffer_entry();
+  m_hw_cfg = hw_cfg;
+  m_tracer = tracer;
+  occupied.reset();
+  active_insts_in_pipeline = 0;
+}
+
+pipelined_simd_unit::~pipelined_simd_unit() {
+  for (unsigned i = 0; i < m_pipeline_depth; i++) {
+    delete m_pipeline_reg[i];
+  }
+  delete m_dispatch_reg;
+}
+
+bool pipelined_simd_unit::can_issue(unsigned latency) const {
+  return !m_dispatch_reg->m_valid && !occupied.test(latency);
+}
+
+void pipelined_simd_unit::issue(register_set &source_reg) {
+  bool partition_issue =
+    m_hw_cfg->get_sub_core_model() && this->is_issue_partitioned();
+  inst_fetch_buffer_entry **ready_reg =
+    source_reg.get_ready(partition_issue, m_issue_reg_id);
+  // simd_function_unit::issue(source_reg);
+
+  //source_reg即为流水线寄存器，目的是找到一个非空的指令，将其移入m_dispatch_reg。
+  source_reg.move_out_to(partition_issue, this->get_issue_reg_id(),
+                         m_dispatch_reg);
+  //设置m_dispatch_reg的标识占用位图的状态，m_dispatch_reg是warp_inst_t类型，可获取该指令的延迟。
+  occupied.set(m_dispatch_reg->latency);
+}
+
+bool sfu::can_issue(const inst_fetch_buffer_entry &inst) const {
+  unsigned _fetch_instn_id = inst.uid;
+  unsigned _gwid = inst.wid;
+  unsigned _kid = inst.kid;
+  compute_instn* tmp = 
+    m_tracer->get_one_kernel_one_warp_one_instn(_kid, _gwid, _fetch_instn_id);
+  _inst_trace_t* tmp_inst_trace = tmp->inst_trace;
+  if (tmp_inst_trace->get_func_unit() == SFU_UNIT) {
+    unsigned latency = tmp_inst_trace->get_latency();
+    return pipelined_simd_unit::can_issue(latency);
+  } else {
+    return false;
+  }
+}
+
+void sfu::issue(register_set &source_reg) {
+  // inst_fetch_buffer_entry **ready_reg =
+  //     source_reg.get_ready(m_hw_cfg->get_sub_core_model(), m_issue_reg_id);
+  // (*ready_reg)->op_pipe = SFU__OP;
+  pipelined_simd_unit::issue(source_reg);
+}
+
+bool dp_unit::can_issue(const inst_fetch_buffer_entry &inst) const {
+  unsigned _fetch_instn_id = inst.uid;
+  unsigned _gwid = inst.wid;
+  unsigned _kid = inst.kid;
+  compute_instn* tmp = 
+    m_tracer->get_one_kernel_one_warp_one_instn(_kid, _gwid, _fetch_instn_id);
+  _inst_trace_t* tmp_inst_trace = tmp->inst_trace;
+  if (tmp_inst_trace->get_func_unit() == DP_UNIT) {
+    unsigned latency = tmp_inst_trace->get_latency();
+    return pipelined_simd_unit::can_issue(latency);
+  } else {
+    return false;
+  }
+}
+
+void dp_unit::issue(register_set &source_reg) {
+  // inst_fetch_buffer_entry **ready_reg =
+  //     source_reg.get_ready(m_hw_cfg->get_sub_core_model(), m_issue_reg_id);
+  // (*ready_reg)->op_pipe = DP___OP;
+  pipelined_simd_unit::issue(source_reg);
+}
+
+bool sp_unit::can_issue(const inst_fetch_buffer_entry &inst) const {
+  unsigned _fetch_instn_id = inst.uid;
+  unsigned _gwid = inst.wid;
+  unsigned _kid = inst.kid;
+  compute_instn* tmp = 
+    m_tracer->get_one_kernel_one_warp_one_instn(_kid, _gwid, _fetch_instn_id);
+  _inst_trace_t* tmp_inst_trace = tmp->inst_trace;
+  if (tmp_inst_trace->get_func_unit() == SP_UNIT) {
+    unsigned latency = tmp_inst_trace->get_latency();
+    return pipelined_simd_unit::can_issue(latency);
+  } else {
+    return false;
+  }
+}
+
+void sp_unit::issue(register_set &source_reg) {
+  // inst_fetch_buffer_entry **ready_reg =
+  //     source_reg.get_ready(m_hw_cfg->get_sub_core_model(), m_issue_reg_id);
+  // (*ready_reg)->op_pipe = SP___OP;
+  pipelined_simd_unit::issue(source_reg);
+}
+
+bool tensor_core::can_issue(const inst_fetch_buffer_entry &inst) const {
+  unsigned _fetch_instn_id = inst.uid;
+  unsigned _gwid = inst.wid;
+  unsigned _kid = inst.kid;
+  compute_instn* tmp = 
+    m_tracer->get_one_kernel_one_warp_one_instn(_kid, _gwid, _fetch_instn_id);
+  _inst_trace_t* tmp_inst_trace = tmp->inst_trace;
+  if (tmp_inst_trace->get_func_unit() == TENSOR_CORE_UNIT) {
+    unsigned latency = tmp_inst_trace->get_latency();
+    return pipelined_simd_unit::can_issue(latency);
+  } else {
+    return false;
+  }
+}
+
+void tensor_core::issue(register_set &source_reg) {
+  // inst_fetch_buffer_entry **ready_reg =
+  //     source_reg.get_ready(m_hw_cfg->get_sub_core_model(), m_issue_reg_id);
+  // (*ready_reg)->op_pipe = TENSOR_OP;
+  pipelined_simd_unit::issue(source_reg);
+}
+
+bool int_unit::can_issue(const inst_fetch_buffer_entry &inst) const {
+  unsigned _fetch_instn_id = inst.uid;
+  unsigned _gwid = inst.wid;
+  unsigned _kid = inst.kid;
+  compute_instn* tmp = 
+    m_tracer->get_one_kernel_one_warp_one_instn(_kid, _gwid, _fetch_instn_id);
+  _inst_trace_t* tmp_inst_trace = tmp->inst_trace;
+  if (tmp_inst_trace->get_func_unit() == INT_UNIT) {
+    unsigned latency = tmp_inst_trace->get_latency();
+    return pipelined_simd_unit::can_issue(latency);
+  } else {
+    return false;
+  }
+}
+
+void int_unit::issue(register_set &source_reg) {
+  // inst_fetch_buffer_entry **ready_reg =
+  //     source_reg.get_ready(m_hw_cfg->get_sub_core_model(), m_issue_reg_id);
+  // (*ready_reg)->op_pipe = INT__OP;
+  pipelined_simd_unit::issue(source_reg);
+}
+
+bool specialized_unit::can_issue(const inst_fetch_buffer_entry &inst) const {
+  unsigned _fetch_instn_id = inst.uid;
+  unsigned _gwid = inst.wid;
+  unsigned _kid = inst.kid;
+  compute_instn* tmp = 
+    m_tracer->get_one_kernel_one_warp_one_instn(_kid, _gwid, _fetch_instn_id);
+  _inst_trace_t* tmp_inst_trace = tmp->inst_trace;
+
+  bool op_condition = false;
+
+  switch (m_index) {
+    case 0:
+      op_condition = (tmp_inst_trace->get_func_unit() == SPEC_UNIT_1);
+      break;
+    case 1:
+      op_condition = (tmp_inst_trace->get_func_unit() == SPEC_UNIT_2);
+      break;
+    case 2:
+      op_condition = (tmp_inst_trace->get_func_unit() == SPEC_UNIT_3);
+      break;
+    default:
+      return false;
+      break;
+  }
+
+  if (op_condition) {
+    unsigned latency = tmp_inst_trace->get_latency();
+    return pipelined_simd_unit::can_issue(latency);
+  } else {
+    return false;
+  }
+}
+
+void specialized_unit::issue(register_set &source_reg) {
+  // inst_fetch_buffer_entry **ready_reg =
+  //     source_reg.get_ready(m_hw_cfg->get_sub_core_model(), m_issue_reg_id);
+  // (*ready_reg)->op_pipe = SPECIALIZED_UNIT_1_OP + m_index;
+  pipelined_simd_unit::issue(source_reg);
+}
+
+bool mem_unit::can_issue(const inst_fetch_buffer_entry &inst) const {
+  unsigned _fetch_instn_id = inst.uid;
+  unsigned _gwid = inst.wid;
+  unsigned _kid = inst.kid;
+  compute_instn* tmp = 
+    m_tracer->get_one_kernel_one_warp_one_instn(_kid, _gwid, _fetch_instn_id);
+  _inst_trace_t* tmp_inst_trace = tmp->inst_trace;
+  if (tmp_inst_trace->get_func_unit() == LDST_UNIT) {
+    unsigned latency = tmp_inst_trace->get_latency();
+    return pipelined_simd_unit::can_issue(latency);
+  } else {
+    return false;
+  }
+}
+
+void mem_unit::issue(register_set &source_reg) {
+  // inst_fetch_buffer_entry **ready_reg =
+  //     source_reg.get_ready(m_hw_cfg->get_sub_core_model(), m_issue_reg_id);
+  // (*ready_reg)->op_pipe = MEM__OP;
+  pipelined_simd_unit::issue(source_reg);
+}
